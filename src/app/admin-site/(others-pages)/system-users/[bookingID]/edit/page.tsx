00152e785f8ed349e44c0605c26d98ef22f24400
DevOduk <austine.oduk577@gmail.com>
2026-06-30 20:27:01 -0400

    June Update

--- src/app/admin-site/(others-pages)/system-users/[bookingID]/edit/page.tsx
+++ src/app/admin-site/(others-pages)/system-users/[bookingID]/edit/page.tsx
@@ -3,8 +3,7 @@ import EditBookingForm from '@/components/bookings/EditBooking';
 import BookingNotFound from '@/components/bookings/NotFound';
 import PageBreadcrumb from '@/components/common/PageBreadCrumb';
 import Button from '@/components/ui/button/Button';
-import { useFleet } from '@/context/FleetContext';
-import { bookings } from '@/data/mockFleetData';
+import { useBooking } from '@/context/BookingContext';
 import { ChevronLeftIcon } from '@/icons';
 import Link from 'next/link';
 import { use } from 'react';
@@ -15,42 +14,43 @@ interface VehiclePageProps {
 
 
 const EditBookingsPage = ({ params }: VehiclePageProps) => {
-const resolvedParams = use(params);
+  const resolvedParams = use(params);
+  const { bookings } = useBooking();
 
   const bookingID = resolvedParams.bookingID;
-    const bookingDetails = bookings.find(b => b.id === parseInt(bookingID));
+  const bookingDetails = bookings.find(b => b.id === parseInt(bookingID));
 
   if (!bookingDetails) {
     return <BookingNotFound />
   }
 
-  
-const breadcrumbItems = [
-  { label: "Bookings", href: "/bookings" },
-  { label: "Booking "+bookingID, href: "/bookings/"+bookingID },
-];
+
+  const breadcrumbItems = [
+    { label: "Bookings", href: "/bookings" },
+    { label: "Booking " + bookingID, href: "/bookings/" + bookingID },
+  ];
 
   return (
     <main className="space-y-6 p-6">
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/3 lg:p-6">
-      {/* this breadcrumb should show home / bookings / create new booking */}
+        {/* this breadcrumb should show home / bookings / create new booking */}
 
-            <PageBreadcrumb items={breadcrumbItems} pageTitle="Edit Booking" />
+        <PageBreadcrumb items={breadcrumbItems} pageTitle="Edit Booking" />
 
-      <div className="flex gap-3 items-center mb-4">
+        <div className="flex gap-3 items-center mb-4">
 
-       <Link href={"/bookings/"+bookingID} className="mr-2">
-                <Button size="sm" variant="danger-outline">
-                  <ChevronLeftIcon />
-                  Back to Booking
-                </Button>
-              </Link>
-        <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
-          Edit Booking {bookingID}
-        </h3>
-      </div>
+          <Link href={"/bookings/" + bookingID} className="mr-2">
+            <Button size="sm" variant="danger-outline">
+              <ChevronLeftIcon />
+              Back to Booking
+            </Button>
+          </Link>
+          <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
+            Edit Booking {bookingID}
+          </h3>
+        </div>
         <div className="space-y-6">
-<EditBookingForm id={parseInt(bookingID)} />
+          <EditBookingForm id={parseInt(bookingID)} />
         </div>
       </div>
     </main>
