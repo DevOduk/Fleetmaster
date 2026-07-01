354377ad4847d3586e138a87b9fecfac1af5df8b
DevOduk <austine.oduk577@gmail.com>
2026-07-01 11:26:39 -0400

    June Update

--- src/app/tenant-manager/(other-pages)/view-feedback/page.tsx
+++ src/app/tenant-manager/(other-pages)/view-feedback/page.tsx
@@ -1,7 +1,7 @@
 // src/app/admin-site/(others-pages)/feedbacks/page.tsx
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import { createClient } from "@/utils/supabase/server";
-import ViewFeedbacks from "@/components/feedback/ViewFeedbacks";
+import ViewFeedbacks, { FeedbackLog } from "@/components/feedback/ViewFeedbacks";
 
 export const metadata = {
   title: "User Feedbacks Directory | FleetManager",
@@ -24,13 +24,13 @@ async function getAllFeedbacks() {
         category, 
         feedback_text, 
         created_at,
-tenant:fleetmaster_tenants!tenant_id (
+      tenant:fleetmaster_tenants!tenant_id (
       name,
       slug,
       about,
       email
     ),
-sender:fleetmaster_admins!user_id (
+    sender:fleetmaster_admins!user_id (
       first_name,
       last_name,
       profile_pic,
@@ -44,7 +44,14 @@ sender:fleetmaster_admins!user_id (
       return [];
     }
 
-    return data || [];
+    const formattedFeedbacks: FeedbackLog[] = (data || []).map((item) => ({
+      ...item,
+      // Force tenant and sender to be objects instead of arrays
+      tenant: Array.isArray(item.tenant) ? item.tenant[0] : item.tenant,
+      sender: Array.isArray(item.sender) ? item.sender[0] : item.sender,
+    }));
+
+    return formattedFeedbacks || [];
   } catch (err) {
     console.error("Failed to fetch feedback logs:", err);
     return [];
