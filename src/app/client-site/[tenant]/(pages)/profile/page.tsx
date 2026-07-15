3fc6bfa3d2fdcff1b6a105c829acda317e917ef5
DevOduk <austine.oduk577@gmail.com>
2026-07-15 17:47:17 -0400

    July Update

--- src/app/client-site/[tenant]/(pages)/profile/page.tsx
+++ src/app/client-site/[tenant]/(pages)/profile/page.tsx
@@ -1,9 +1,9 @@
+import ProfilePage from "@/components/ProfilePage/client-profile/ProfilePage";
 import { Metadata } from "next";
-import ProfilePage from "@/components/user-profile/ProfilePage/client-profile/ProfilePage";
 
 export const metadata: Metadata = {
   title:
-    "View Profile | FleetManager Admin Dashboard - Best tool for Fleet Management",
+    "Profile | FleetManager Admin Dashboard - Best tool for Fleet Management",
   description: "FleetManager is the ultimate fleet management dashboard built with Next.js and Tailwind CSS. Monitor your fleet's performance, track vehicles in real-time, and optimize operations with our intuitive interface. Try it now and experience seamless fleet management like never before.",
 };
 
