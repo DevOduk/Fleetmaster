f339e3d4f244d27270e84fbc050482d81d988ed5
DevOduk <austine.oduk577@gmail.com>
2026-08-07 05:36:06 -0400

    July Update

--- src/app/admin-site/layout.tsx
+++ src/app/admin-site/layout.tsx
@@ -6,24 +6,31 @@ import { SidebarProvider } from "@/context/SidebarContext";
 import React from "react";
 import { AdminFleetProvider } from "@/context/AdminFleetContext";
 import { AdminBookingProvider } from "@/context/AdminBookingContext";
+import { headers } from "next/headers";
 
-export default function RootAdminLayout({
+export default async function RootAdminLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const headerList = await headers();
+    const tenantDataRaw = headerList.get('x-tenant-data');
+
+    // 1. Resolve tenant details instantly on the server
+    const tenantData = JSON.parse(tenantDataRaw);
+
     return (
-        <UserProvider>
-                <SettingsProvider>
-                    <AdminFleetProvider>
-                        <AdminBookingProvider>
-                            <SidebarProvider>
-                                {/* No UI elements here, just raw children */}
-                                {children}
-                            </SidebarProvider>
-                        </AdminBookingProvider>
-                    </AdminFleetProvider>
-                </SettingsProvider>
+        <UserProvider initialUser={tenantData}>
+            <SettingsProvider>
+                <AdminFleetProvider>
+                    <AdminBookingProvider>
+                        <SidebarProvider>
+                            {/* No UI elements here, just raw children */}
+                            {children}
+                        </SidebarProvider>
+                    </AdminBookingProvider>
+                </AdminFleetProvider>
+            </SettingsProvider>
         </UserProvider>
     );
 }
