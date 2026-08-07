bd105eaac4f11af975a34e64c40b54692acf4ba8
DevOduk <austine.oduk577@gmail.com>
2026-08-07 05:27:52 -0400

    July Update

--- src/app/(full-width-pages)/layout.tsx
+++ src/app/(full-width-pages)/layout.tsx
@@ -1,6 +1,5 @@
 import { AdminProvider } from "@/context/AdminContext";
 import { TenantProvider } from "@/context/TenantContext";
-import { UserProvider } from "@/context/UserContext";
 
 export default function FullWidthPageLayout({
   children,
@@ -10,9 +9,7 @@ export default function FullWidthPageLayout({
   return (
     <TenantProvider>
       <AdminProvider>
-        <UserProvider>
           <div>{children}</div>
-        </UserProvider>
       </AdminProvider>
     </TenantProvider>
   )
